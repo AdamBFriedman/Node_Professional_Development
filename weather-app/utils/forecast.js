4805684021503d0ef8b80b60ace8ca64bc5025ae
Adam Friedman <adam.friedman5@gmail.com>
2021-12-14 15:43:02 -0600

    Destructure functions

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,17 +2,17 @@ const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
   const forecastUrl = `http://api.weatherstack.com/current?access_key=fd59d2c58a44395fbec0b5e035f18858&query=${latitude},${longitude}&units=f`;
-  request({ url: forecastUrl, json: true }, (error, response) => {
+  request({ url: forecastUrl, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to the weather service!", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("Unable to find location.  Try another search.", undefined);
     } else {
       callback(
         undefined,
-        `It is currently ${response.body.current.weather_descriptions[0].toLowerCase()} outside. The temperature is ${
-          response.body.current.temperature
-        } degrees but it feels like ${response.body.current.feelslike} degrees.`
+        `It is currently ${body.current.weather_descriptions[0].toLowerCase()} outside. The temperature is ${
+          body.current.temperature
+        } degrees but it feels like ${body.current.feelslike} degrees.`
       );
     }
   });
