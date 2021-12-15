d867392e5e8634649e9d6956bae58fea0fb762cf
Adam Friedman <adam.friedman5@gmail.com>
2021-12-15 10:28:53 -0600

    Now serving html pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.get("", (req, res) => {
-  res.send("Hello beautiful");
-});
-
-app.get("/help", (req, res) => {
-  res.send("Help Page");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About Page");
-});
-
-app.get("/weather", (req, res) => {
-  res.send("Weather Page");
-});
+app.use(express.static(publicDirectoryPath));
 
 app.listen(3000, () => {
   console.log("Server started");
