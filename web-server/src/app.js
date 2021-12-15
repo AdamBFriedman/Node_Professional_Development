8da8f60fef0c59c7b44c2ec001c70879531c54f1
Adam Friedman <adam.friedman5@gmail.com>
2021-12-15 11:27:50 -0600

    Configure app to point to templates folder.  Added comments to prior code

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,12 +2,16 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
 
-// Set up Handlebars
+// Set up Handlebars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
 
-// Set up static path
+// Set up static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
